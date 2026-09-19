d71ff8cd07d0e1c03aa2f90cca55f48fb98faed6
FakeAbid11 <183648279+FakeAbid11@users.noreply.github.com>
2026-09-19 16:46:09 +0600

    gitignore local CI-watch helpers

--- watch_ci.ps1
+++
@@ -1,6 +0,0 @@
-$repo = 'FakeAbid11/LumoVault-2'
-$id = (gh run list --repo $repo --limit 1 --json databaseId | ConvertFrom-Json).databaseId
-gh run watch $id --repo $repo --exit-status --interval 15 | Out-Null
-$code = $LASTEXITCODE
-gh run view $id --repo $repo --json conclusion,status | Set-Content ci_result.json
-exit $code
